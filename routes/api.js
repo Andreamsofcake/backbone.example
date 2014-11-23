17442f983879a0931e3ad3d64ad66d2018ec1467
“Andreamsofcake” <andrea.moulding@gmail.com>
2014-11-22 21:41:49 -0800

    working on api stuff

--- routes/api.js
+++ routes/api.js
@@ -6,10 +6,13 @@ var router = express.Router();
 //Models
 var Product = require('../models/product');
 
-
 //routes
-Product.methods(['get', 'put', 'post', 'delete'])
-Product.register(router, '/products')
+Product.methods(['get','put', 'post', 'delete'])
+  .after('get', function(req, res, next){
+  Product.find({}).sort({created_at: -1});
+  next();
+});
+Product.register(router, '/products');
 
 
 // return router
